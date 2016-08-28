19a6af426af8a701fcf6d34a753e184b8c679fe5
Konstantin Rebrov <krebrov001@student.butte.edu>
2016-08-28 19:43:27 +0000

    Lab 1 ASCII art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,46 @@
+/* Konstantin Rebrov
+ * 1.1 Lab - ASCII Art
+ * 8/28/2016
+ * This program prints out
+ * a Russian dome.
+ */
+
+
+#include <iostream> // imports the input stream
+using namespace std; // imports the << operator
+
+int main() // the main method
+{
+    cout << "Russian dome" << endl;
+    cout << "               +" << endl;
+    cout << "             ==|==" << endl;
+    cout << "              -|-" << endl;
+    cout << "               |" << endl;
+    cout << "              /|\\ " << endl;
+    cout << "             /A A\\ " << endl;
+    cout << "            /V V V\\ " << endl;
+    cout << "           /V V V V\\ " << endl;
+    cout << "         / V V V V V \\ " << endl;
+    cout << "       /V V V V V V V V\\ " << endl;
+    cout << "      (V V V V V V V V V)" << endl;
+    cout << "       \\V V V V V V V V/" << endl;
+    cout << "        \\V V V V V V V/" << endl;
+    cout << "          \\ V V V V /" << endl;
+    cout << "           |=======|" << endl;
+    cout << "           | | | | |" << endl;
+    cout << "           | | | | |" << endl;
+    cout << "           | | | | |" << endl;
+    cout << "           |/ \\ / \\|" << endl;
+    cout << "           | A V A |" << endl;
+    cout << "          |++++A++++|" << endl;
+    cout << "         /----/ \\----\\ " << endl;
+    cout << "   |---/UUUUU/ # \\UUUUU\\---|" << endl;
+    cout << "   |^^/     /  #  \\     \\^^|" << endl;
+    cout << "   |^/    /   |+|   \\    \\^|" << endl;
+    cout << "   |/  /    |+++++|    \\  \\|" << endl;
+    cout << "   | / *  A |     | A *  \\ |" << endl;
+    cout << "   |/ *  / \\|     |/ \\ *  \\|" << endl;
+    cout << "   | *  |   |^^^^^|   | *  |" << endl;
+    
+    return 0;
+}
