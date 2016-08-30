bcf1eb2942a1406f0970a69a898ec45c79462d55
Konstantin Rebrov <krebrov001@student.butte.edu>
2016-08-30 06:00:19 +0000

    Lab 2 Submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,39 @@
+/**
+ * This program was created by Konstantin Rebrov.
+ * Date: 8/29/2016
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Declare three int variables num1, num2, and score.
+    Assign 10 to score.
+    
+    Declare one boolean variable status.
+    Assign true to status.
+    
+    Assign a random number between 0 and 10 to num1.
+    
+    While status is true: 
+    
+        Assign the user's input to num2.
+    
+        If num1 is equal to num2:
+            Print "You win" to the screen.
+            Assign score + 10 to score.
+            Assign false to status.
+    
+        If num1 is greater than num2:
+            Print "Lower" to the screen.
+            Assign score - 1 to score.
+    
+        If num1 is less than num2:
+            Print "Higher" to the screen.
+            Assign score - 1 to score.
+    
+    End the program.
+    */
+}
