515b4b1c245ca85f9518d60508a85ea453984844
Konstantin Rebrov <krebrov001@student.butte.edu>
2016-10-04 20:50:10 +0000

    Lab 10 Submission

--- lab10/lab10.cpp
+++ lab10/lab10.cpp
@@ -132,71 +132,71 @@ int main() {
     cout << "." << endl;
     // Automatically check my answer.
     cout << "My answer is ";
-    cout << CheckAnswer(function1, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function1(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 2 evaluates to " << function2() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 2 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function2, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function2(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 3 evaluates to " << function3() << "." << endl;
     answersList.SetMyAnswer(false);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 3 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function3, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function3(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 4 evaluates to " << function4() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 4 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function4, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function4(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 5 evaluates to " << function5() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 5 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function5, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function5(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 6 evaluates to " << function6() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 6 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function6, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function6(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 7 evaluates to " << function7() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 7 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function7, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function7(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 8 evaluates to " << function8() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 8 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function8, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function8(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     cout << "Equation 9 evaluates to " << function9() << "." << endl;
     answersList.SetMyAnswer(true);
-    cout << "My answer for equation 1 is " << answersList.GetMyAnswer();
+    cout << "My answer for equation 9 is " << answersList.GetMyAnswer();
     cout << "." << endl;
     cout << "My answer is ";
-    cout << CheckAnswer(function9, answersList.GetMyAnswer()) << "." << endl;
+    cout << CheckAnswer(function9(), answersList.GetMyAnswer()) << "." << endl;
     cout << endl;
     
     return 0;
